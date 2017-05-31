1093976d110a655fbc3d13f2286fccb1468535e1
DRickard <drickard1967@ad.library.ucla.edu>
2017-05-31 13:47:02 -0700

    WS-1009: adding annotations produces docs

--- StatsServices/src/main/java/edu/ucla/library/libservices/pubstats/web/service/LookupService.java
+++ StatsServices/src/main/java/edu/ucla/library/libservices/pubstats/web/service/LookupService.java
@@ -2,16 +2,20 @@ package edu.ucla.library.libservices.pubstats.web.service;
 
 import edu.ucla.library.libservices.pubstats.generators.UnitPointGenerator;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 import javax.servlet.ServletConfig;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+@Api(value = "/lookup", description = "Lookup operations on db")
 @Path( "/lookup" )
 public class LookupService
 {
@@ -29,7 +33,11 @@ public class LookupService
   @GET
   @Produces( "application/json" )
   @Path( "/units/{unitID}" )
-  public Response getUnits( @PathParam( "unitID" ) String unitID )
+  @ApiOperation(value = "Finds Library units/service points",
+    notes = "Valid unitID values are full_units, full_points, unit ID from db",
+    response = UnitPointGenerator.class,
+    responseContainer = "List")
+  public Response getUnits( @ApiParam(value = "unit(s) to be retrieved", required = true)  @PathParam( "unitID" ) String unitID )
   {
     UnitPointGenerator docMaker;
 
@@ -41,7 +49,7 @@ public class LookupService
       case ALL_UNITS:
         docMaker.populateUnits();
         break;
-      case ALL_POINTS:
+      case ALL_POINTS: 
         docMaker.populateAllUnitPoints();
         break;
       default:
--- StatsServices/src/main/java/edu/ucla/library/libservices/pubstats/web/service/SubmitService.java
+++ StatsServices/src/main/java/edu/ucla/library/libservices/pubstats/web/service/SubmitService.java
@@ -1,17 +1,22 @@
 package edu.ucla.library.libservices.pubstats.web.service;
 
 import edu.ucla.library.libservices.pubstats.beans.Submission;
-
 import edu.ucla.library.libservices.pubstats.handlers.SubmissionHandler;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import javax.servlet.ServletConfig;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+@Api(value = "/stats", description = "Submit data to db")
 @Path( "/stats/" )
 public class SubmitService
 {
@@ -26,6 +31,10 @@ public class SubmitService
   @PUT
   @Consumes( "application/json" )
   @Path( "submit" )
+  @ApiOperation(value = "Submits statistics to db",
+    notes = "Consumes a Submission object")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Stats saved"),
+      @ApiResponse(code = 500, message = "Stats insertion failed: ")})
   public Response submitStats(Submission theStats)
   {
     SubmissionHandler handler;
@@ -36,7 +45,7 @@ public class SubmitService
     try
     {
       handler.submitStats();
-      return Response.ok().build();
+      return Response.ok().entity( "Stats saved" ).build();
     }
     catch ( Exception e )
     {
